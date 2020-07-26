f7ff01f6c02ac11c495fc53d98c35f6dd52809be
home <int.config@gmail.com>
2020-07-26 15:06:04 +0000

    pwm enabled

--- main.py
+++ main.py
@@ -7,6 +7,30 @@ def init():
 	GPIO.setup(13,GPIO.OUT)
 	GPIO.setup(15,GPIO.OUT)
 	GPIO.setup(16,GPIO.OUT)
+	GPIO.setup(32,GPIO.OUT)
+	GPIO.setup(33,GPIO.OUT)
+	f1  = GPIO.PWM(32,100)
+	f2  = GPIO.PWM(33,100)
+	f1.start(0)
+	f2.start(0)
+	return f1,f2
+
+def speed(s,f1,f2):
+	if(s=='vlow'):
+		f1.ChangeDutyCycle(10)
+		f2.ChangeDutyCycle(10)
+	if(s=='low'):
+		f1.ChangeDutyCycle(25)
+		f2.ChangeDutyCycle(25)
+	if(s=='high'):
+		f1.ChangeDutyCycle(75)
+		f2.ChangeDutyCycle(75)
+	if(s=='vhigh'):
+		f1.ChangeDutyCycle(100)
+		f2.ChangeDutyCycle(100)
+	if(s=='med'):
+		f1.ChangeDutyCycle(50)
+		f2.ChangeDutyCycle(50)
 
 def reset():
 	GPIO.output(11,GPIO.LOW)
@@ -48,10 +72,27 @@ def right(t):
 
 def clean_exit():
 	GPIO.cleanup()
+	f1.stop()
+	f2.stop()
 
-init()
-forward(1)
-reverse(1)
-left(1)
-right(1)
+f1,f2 = init()
+speed('vlow',f1,f2)
+forward(2)
+
+speed('low',f1,f2)
+forward(2)
+
+
+speed('med',f1,f2)
+forward(2)
+
+speed('high',f1,f2)
+forward(2)
+
+speed('vhigh',f1,f2)
+forward(2)
+
+#reverse(1)
+#left(1)
+#right(1)
 clean_exit()
