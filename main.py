d27af94a9f50466c87d3b10dacbf4611c152c300
home <int.config@gmail.com>
2020-07-26 12:23:47 +0000

    added functions

--- main.py
+++ main.py
@@ -1,36 +1,57 @@
 import RPi.GPIO as GPIO
 import time as time
 
-GPIO.setmode(GPIO.BOARD)
+def init():
+	GPIO.setmode(GPIO.BOARD)
+	GPIO.setup(11,GPIO.OUT)
+	GPIO.setup(13,GPIO.OUT)
+	GPIO.setup(15,GPIO.OUT)
+	GPIO.setup(16,GPIO.OUT)
 
-GPIO.setup(11,GPIO.OUT)
-GPIO.setup(13,GPIO.OUT)
-GPIO.setup(15,GPIO.OUT)
-GPIO.setup(16,GPIO.OUT)
+def reset():
+	GPIO.output(11,GPIO.LOW)
+	GPIO.output(13,GPIO.LOW)
+	GPIO.output(15,GPIO.LOW)
+	GPIO.output(16,GPIO.LOW)
 
+def forward(t):
+	reset()
+	GPIO.output(11,GPIO.HIGH)
+	GPIO.output(13,GPIO.LOW)
+	GPIO.output(15,GPIO.HIGH)
+	GPIO.output(16,GPIO.LOW)
+	time.sleep(t)
 
+def reverse(t):
+	reset()
+	GPIO.output(11,GPIO.LOW)
+	GPIO.output(13,GPIO.HIGH)
+	GPIO.output(15,GPIO.LOW)
+	GPIO.output(16,GPIO.HIGH)
+	time.sleep(t)
 
-GPIO.output(11,GPIO.LOW)
-GPIO.output(13,GPIO.LOW)
-GPIO.output(15,GPIO.LOW)
-GPIO.output(16,GPIO.LOW)
+def left(t):
+	reset()
+	GPIO.output(11,GPIO.LOW)
+	GPIO.output(13,GPIO.HIGH)
+	GPIO.output(15,GPIO.HIGH)
+	GPIO.output(16,GPIO.LOW)
+	time.sleep(t)
 
-GPIO.output(11,GPIO.HIGH)
-GPIO.output(13,GPIO.LOW)
-GPIO.output(15,GPIO.HIGH)
-GPIO.output(16,GPIO.LOW)
+def right(t):
+	reset()
+	GPIO.output(11,GPIO.HIGH)
+	GPIO.output(13,GPIO.LOW)
+	GPIO.output(15,GPIO.LOW)
+	GPIO.output(16,GPIO.HIGH)
+	time.sleep(t)
 
-time.sleep(3)
+def clean_exit():
+	GPIO.cleanup()
 
-GPIO.output(11,GPIO.LOW)
-GPIO.output(13,GPIO.LOW)
-GPIO.output(15,GPIO.LOW)
-GPIO.output(16,GPIO.LOW)
-
-GPIO.output(11,GPIO.LOW)
-GPIO.output(13,GPIO.HIGH)
-GPIO.output(15,GPIO.LOW)
-GPIO.output(16,GPIO.HIGH)
-
-time.sleep(3)
-GPIO.cleanup()
+init()
+forward(1)
+reverse(1)
+left(1)
+right(1)
+clean_exit()
